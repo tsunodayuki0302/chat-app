36ee228faa4442caeece0b95ed4b43a5c31814a3
tsunodayuki0302 <tsunoda.yuki0302@gmail.com>
2021-09-08 12:46:45 +0900

    room_usersテーブルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,9 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
 
+  has_many :room_users
+  has_many :rooms, through: :room_users
   validates :name, presence: true
 end
